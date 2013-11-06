a81056856d92bce40f87abaee083d78978f4c30e
Jim <jdurand@o2web.ca>
2013-11-05 20:21:07 -0500

    Icluded taxes in free shipping calculator

--- core/app/models/spree/calculator/free_shipping.rb
+++ core/app/models/spree/calculator/free_shipping.rb
@@ -11,8 +11,27 @@ module Spree
       else
         order = object
       end
+      
+      (order.ship_total + ship_taxes(order))
+    end
 
-      order.ship_total
+    def ship_taxes(order)
+      tax_total = 0
+      order.adjustments.tax.each do |adjustment|
+        tax_rate = adjustment.originator
+        return false unless tax_rate.tax_category && tax_rate.tax_category.is_default
+        order.adjustments.shipping.each do |shipping_adjustment|
+          shipping_calculator = shipping_adjustment.originator.calculator
+          shipping_fee = shipping_calculator.compute(order)
+          if(shipping_calculator.preferred_taxable)
+            tax_amount = shipping_fee * tax_rate.amount
+            if tax_rate.zone.contains? order.tax_zone
+              tax_total += tax_amount
+            end
+          end
+        end
+      end
+      tax_total
     end
   end
 end
